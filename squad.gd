7ee127ed0f9d24c87bdd8f3124193651af87f827
avelitj12 <avelitj12@gmail.com>
2024-01-15 13:19:37 -0700

    Add base move speed placeholder

--- squad.gd
+++ squad.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 signal selected
 
-const INFESTATION_FIGHT_RATE = -.05
+const BASE_INFESTATION_FIGHT_RATE = -.05
+const BASE_MOVE_SPEED = 100 # TODO: Determine best value for this constant
 
 var location: Area2D
 
@@ -10,12 +11,12 @@ func _on_area_entered(area):
 	location = area
 	if location && location.has_method("add_infestation_modifier"):
 		# TEMP: Setting to 3x fight rate so single squad can fight back infestation
-		location.add_infestation_modifier(INFESTATION_FIGHT_RATE * 3)
+		location.add_infestation_modifier(BASE_INFESTATION_FIGHT_RATE * 3)
 
 func _on_area_exited(area):
 	if location && location.has_method("add_infestation_modifier"):
 		# TEMP: Setting to 3x fight rate so single squad can fight back infestation
-		location.add_infestation_modifier(-INFESTATION_FIGHT_RATE * 3)
+		location.add_infestation_modifier(-BASE_INFESTATION_FIGHT_RATE * 3)
 	
 	location = null
 
