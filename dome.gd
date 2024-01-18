0f8c22a0fef17db5914710ec7aed3b62cc62a0a8
Cameron Avelis <cmavelis@gmail.com>
2024-01-17 16:20:42 -0800

    rarer random infestation

--- dome.gd
+++ dome.gd
@@ -30,7 +30,7 @@ func _on_infestation_check_timer_timeout():
 			infestation_stage = InfestationStage.UNINFESTED
 			$DomeStatus.text = "Safe"
 			infestation_rate = 0.0
-		if randf() < .1 && infestation_rate == 0: # Temp, infestation should be initiated by main eventually
+		if randf() < .01 && infestation_rate == 0: # Temp, infestation should be initiated by main eventually
 				infestation_rate += .1
 	elif infestation_percentage <= .50:
 		if infestation_stage != InfestationStage.MINOR:
