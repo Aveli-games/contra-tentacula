7a9135121f34aee42d71639e4cc750efd98f293e
Cameron Avelis <cmavelis@gmail.com>
2024-01-22 10:51:05 -0800

    pather working

--- autoloads/Pather.gd
+++ autoloads/Pather.gd
@@ -11,28 +11,31 @@ func find_path(start, finish):
 	# does this break backtrack checking?
 	
 	var new_path = [start]
-	return continue_path(new_path, finish)
+	return _continue_path(new_path, finish)
 
-func continue_path(path_array, finish):
+func _continue_path(path_array, finish):
 	var dome_from = path_array[-1]
 	var connections = DomeConnections.get_dome_connections(dome_from)
 
 	for connection in connections:
 		var connected_dome = connection.dome_b
-		var continued_path = path_array.duplicate(true).push(connected_dome)
+		var continued_path = path_array.duplicate(true)
+		continued_path.append(connected_dome)
 		
 		if connected_dome == finish:
-			# path found
+			# path complete
 			return continued_path
 			
 		# check for backtrack
 		if connected_dome in path_array:
 			print('dead end path: ', continued_path)
-			return false
+			pass
+		else:
+			# recurse: with dome_b added to path, do it again
+			var found_path = _continue_path(continued_path, finish) 
+			if found_path:
+				return found_path
 			
-		# recurse: with dome_b added to path, do it again
-		var found_path = continue_path(continued_path, finish) 
-		if found_path:
-			return found_path
+	return false
 	
 
