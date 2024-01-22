348ca4db1ab787c0220a66bf5bd5944364a6d520
Cameron Avelis <cmavelis@gmail.com>
2024-01-21 18:39:28 -0800

    make progress bar changes smoother

--- dome.gd
+++ dome.gd
@@ -19,13 +19,19 @@ var is_hidden: bool = false
 
 func _ready():
 	$ResourceGenerationTimer.start(1) # TODO: Have timer start on game start, not dome spawn
+	$Building/InfestationProgress.value = 0
 
 func _process(delta):
 	# Process infestation progression inependently in dome's infestation check
 	if infestation_percentage > 0:
 		add_infestation(Globals.BASE_DOME_INFESTATION_RATE * delta)
 	
-	$Building/InfestationProgress.value = infestation_percentage * 100
+	var progress_diff = infestation_percentage - $Building/InfestationProgress.value/100
+	var progress_max_speed = 0.01
+	var progress_adjustment = clamp(-progress_max_speed, progress_diff, progress_max_speed)
+	if progress_adjustment != 0:
+		$Building/InfestationProgress.value += progress_adjustment * 100
+		
 
 func _on_infestation_check_timer_timeout():
 	# determine infestation level
