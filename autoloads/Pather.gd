f6cd99bc77a64bef42acf6eead3500f9d2e1d719
Cameron Avelis <cmavelis@gmail.com>
2024-01-25 13:31:12 -0800

    pather working with start paths

--- autoloads/Pather.gd
+++ autoloads/Pather.gd
@@ -2,16 +2,26 @@ extends Node
 
 class_name Pather
 
-# start, finish are Domes
-##TODO: make it so start can be mid-connector, for canceling movement
-func find_path(start: Dome, finish: Dome):
-	# if start is connection
-	# new path = []
-	# connections = [start.dome_a, start.dome_b]
-	# does this break backtrack checking?
-	
-	var working_paths = [[start]]
-	var visited = {start.get_name(): 1} # is there a Set?
+# finish:
+#   Dome you want to path to
+# params:
+#   `start_dome`, which is a single dome to start from
+#   OR `start_paths`: Dome[][], which are path segments to continue searching from
+func find_path(finish: Dome, params = {}):
+	var working_paths = []
+	var visited = {}
+	if params.has('start_dome'):
+		working_paths.append([params.start_dome])
+	elif params.has('start_paths'):
+		working_paths = params.start_paths.duplicate()
+	else: 
+		push_error('No start parameters provided for pathing')
+		return []
+
+	# treat all provided domes as visited for algorithm
+	for path in working_paths:
+		for dome in path:
+			visited[dome.get_name()] = 1
 
 	while working_paths.size() > 0:
 		var path_base = working_paths.pop_front()
@@ -30,12 +40,12 @@ func find_path(start: Dome, finish: Dome):
 			
 			# check for backtrack
 			if connected_dome in path_base:
-				print('backtrack path: ', continued_path)
-				pass
+				#print('backtrack path: ', continued_path)
+				continue
 			
 			if connected_dome.name in visited.keys():
-				print('already visited dome: ', continued_path)
-				pass
+				#print('already visited dome: ', continued_path)
+				continue
 			
 			visited[connected_dome.name] = 1
 			working_paths.append(continued_path)
